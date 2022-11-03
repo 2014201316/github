734f5c0f32be2e798dc6b9ff4dedb2d3803d57cc
粑粑 <117328447+2014201316@users.noreply.github.com>
2022-11-03 15:30:28 +0800

    GitHub访问接口第二次修改

--- src/main/java/cn/bdqn/controller/GitHubController.java
+++ src/main/java/cn/bdqn/controller/GitHubController.java
@@ -8,7 +8,7 @@ package cn.bdqn.controller;
  */
 public class GitHubController {
     public static void main(String[] args) {
-        System.out.println("骆驼");
+        System.out.println("黄天翔");
     }
 
 }
